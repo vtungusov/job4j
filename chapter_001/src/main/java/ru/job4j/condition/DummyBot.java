7bf51a11a8964acc65a4a552a465fb8239d49962
Vasily Tungusov <vasilt86@mail.ru>
2019-07-07 23:49:35 +0700

    4.1. Глупый бот.[#156390]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * @author Vasily Tungusov
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     *
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
