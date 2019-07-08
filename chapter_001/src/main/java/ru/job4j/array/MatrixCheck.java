010bfb5322d09bc2a0d2b4f68646e465e27fd778
Vasily Tungusov <vasilt86@mail.ru>
2019-07-08 23:06:39 +0700

    6.7. Рефакторинг

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,5 +1,9 @@
 package ru.job4j.array;
 
+/**
+ * @author Vasily Tungusov
+ * @version 1.1
+ */
 public class MatrixCheck {
 
     /**
@@ -13,18 +17,11 @@ public class MatrixCheck {
         boolean dgn1Date = data[0][0];
         boolean dgn2Date = data[data.length - 1][0];
 
-        if (data.length % 2 == 0) {
-            for (int i = 0; i < data.length - 1; i++) {
-                if ((data[i][i] != dgn1Date) &&
-                        (data[data.length - i][i] != dgn1Date))
-                    result = false;
-            }
-        } else
-            for (int i = 0; i < data.length - 1; i++) {
-                if ((data[i][i] != dgn1Date) &&
-                        (data[data.length - i][i] != dgn2Date))
-                    result = false;
-            }
+        for (int i = 0; i < data.length - 1; i++) {
+            if ((data[i][i] != dgn1Date) &&
+                    (data[data.length - i][i] != dgn2Date))
+                result = false;
+        }
         return result;
     }
 }
