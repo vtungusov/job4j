955b2bf272d6e088fd0e3e6d5752940b30116f86
Vasily Tungusov <vasilt86@mail.ru>
2019-07-08 12:53:30 +0700

    5.6. Ипотека.[#156406]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для расчета кредита (без суммы за пользование кредитом)
+ * не использовать в реальной жизни!
+ *
+ * @author Vasily Tungusov
+ * @version 1.0
+ */
+public class Mortgage {
+
+    /**
+     * Расчитывает количество лет,
+     * необходимых для погашения кредита
+     *
+     * @param amount общая сумма кредита
+     * @param monthly ежемесячный платеж
+     * @param percent процент по кредиту
+     * @return количество лет
+     */
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        double total = amount + amount * percent / 100;
+        while (total > 0) {
+            total -= 12*monthly;
+            year++;
+        }
+        return year;
+    }
+}
