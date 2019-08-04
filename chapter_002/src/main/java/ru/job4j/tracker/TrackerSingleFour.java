5d59ec6b4e68beff44afee712697ce65e6ed80b4
Vasily Tungusov <vasilt86@mail.ru>
2019-08-05 00:32:22 +0700

    0. Наследование, композиция и агрегация. [#156502]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/TrackerSingleFour.java
@@ -0,0 +1,77 @@
+package ru.job4j.tracker;
+
+public class TrackerSingleFour {
+
+    private Tracker tracker = new Tracker();
+
+    private TrackerSingleFour() {
+    }
+
+    public static TrackerSingleFour getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    /**
+     * Adds Item to Tracker
+     *
+     * @param item add item
+     * @return added item with preset ID
+     */
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    /**
+     * Replace Item in Tracker
+     *
+     * @param id   ID of replace item
+     * @param item new Item
+     * @return replacement success as boolean
+     */
+    public boolean replace(String id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    /**
+     * Delete Item from Tracker
+     *
+     * @param id id of delete item
+     * @return replacement success as boolean
+     */
+    public boolean delete(String id) {
+        return tracker.delete(id);
+    }
+
+    /**
+     * Find all Item in Tracker
+     *
+     * @return array of found items
+     */
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    /**
+     * Find Item by name
+     *
+     * @param key item name
+     * @return array of found items
+     */
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    /**
+     * Find Item by ID
+     *
+     * @param id item ID
+     * @return found item
+     */
+    public Item findById(String id) {
+        return tracker.findById(id);
+    }
+
+    private static class Holder {
+        private static final TrackerSingleFour INSTANCE = new TrackerSingleFour();
+    }
+}
