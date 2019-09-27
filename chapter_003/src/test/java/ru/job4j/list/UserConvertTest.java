3583ff8f74d9a1907e0f4d6db8649fba084d103d
Vasily Tungusov <vasilt86@mail.ru>
2019-09-27 14:24:25 +0700

    1. Collection API Улучшения[#156360]

--- chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
+++ chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
@@ -3,11 +3,12 @@ package ru.job4j.list;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static java.util.Map.entry;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class UserConvertTest {
     @Test
@@ -15,10 +16,11 @@ public class UserConvertTest {
         List<User> userList = new ArrayList<>();
         userList.add(new User(1, "ivan"));
         userList.add(new User(2, "sergei"));
-        HashMap<Integer, User> result = new UserConvert().process(userList);
-        HashMap<Integer, User> expect = new HashMap<>();
-        expect.put(1, new User(1, "ivan"));
-        expect.put(2, new User(2, "sergei"));
+        Map<Integer, User> result = new UserConvert().process(userList);
+        Map<Integer, User> expect = Map.ofEntries(
+                entry(1, new User(1, "ivan")),
+                entry(2, new User(2, "sergei"))
+        );
         assertThat(result, is(expect));
     }
 }
